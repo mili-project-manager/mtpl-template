8011607192295d473171b704276fea5d3227d08d
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-10-15 22:26:20 +0800

    feat: init template

---
+++ index.js
@@ -0,0 +1,29 @@
+// path of template files
+exports.path = './template'
+
+// mili version >= 1.0.0
+exports.engines = ">=1.0.0<2.0.0"
+
+
+exports.rules = [
+  {
+    path: './template',
+    upgrade: 'keep',
+  },
+  {
+    path: './index.js',
+    upgrade: 'keep',
+  },
+  {
+    path: 'package.json.mustance',
+    upgrade: 'merge',
+    handlers: ['mustance']
+  },
+  {
+    path: 'README.md.mustache',
+    handlers: [
+      'mustance',
+      // core => core.extractHtmlCustomArea('custom')
+    ],
+  },
+]
